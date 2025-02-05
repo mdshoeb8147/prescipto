ea36900ce8bc274bb10a3c5ad4b9305100a30793
mdshoeb8147 <mdshoeb8147@gmail.com>
2025-02-05 13:44:42 +0530

    bug in doctors.jsx

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,4 +1,57 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { doctors } from "../assets/assets";
+// import { AppContext } from "../context/AppContext";
+
 const Doctors = () => {
-  return <div>Doctors</div>;
+  const navigate = useNavigate();
+  const { speciality } = useParams();
+  const [filterDoc, setFilterDoc] = useState([]);
+  // const { doctors } = useContext(AppContext);
+
+  const applyFilter = () => {
+    if (speciality) {
+      setFilterDoc(doctors.filter((doc) => doc.speciality === speciality));
+    } else {
+      setFilterDoc(doctors);
+    }
+  };
+
+  useEffect(() => {
+    applyFilter();
+  }, [doctors, speciality]);
+
+  return (
+    <div>
+      <p>Browse through the doctors specialist</p>
+      <div className="">
+        <div className="">
+          <p>General physician</p>
+          <p>Gynecologist</p>
+          <p>Dermatologist</p>
+          <p>Pediatricians</p>
+          <p>Neurologist</p>
+          <p>Gastroenterologist</p>
+        </div>
+        {filterDoc.map((item, index) => (
+          <div
+            onClick={() => navigate(`/appointment/${item._id}`)}
+            key={index}
+            className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+          >
+            <img src={item.image} alt="" className="bg-blue-50 " />
+            <div className="p-4">
+              <div className="flex items-center gap-2 text-sm text-center text-green-500">
+                <p className="w-2 h-2 bg-green-500 rounded-full"></p>
+                <p>Available</p>
+              </div>
+              <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+              <p className="text-gray-600 text-sm">{item.speciality}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Doctors;
