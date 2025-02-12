9819f3668383d8752f68ce1bbfb6715df5be6b78
mdshoeb8147 <mdshoeb8147@gmail.com>
2025-02-12 13:40:28 +0530

    error resolved

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -1,4 +1,22 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { doctors } from "../assets/assets";
+
 const Appointment = () => {
-  return <div>Appointment</div>;
+  const { docId } = useParams();
+  // const { doctors } = useContext();
+
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find((doc) => doc._id === docId);
+    setDocInfo(docInfo);
+    console.log(docInfo);
+  };
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
+
+  return <div></div>;
 };
 export default Appointment;
