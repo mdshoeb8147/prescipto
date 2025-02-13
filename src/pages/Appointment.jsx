0dc0448dcd4e3827276419c4a9dbbb248371a82c
mdshoeb8147 <mdshoeb8147@gmail.com>
2025-02-13 15:07:07 +0530

    current time increses

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -7,15 +7,65 @@ const Appointment = () => {
   // const { doctors,currencySymbol} = useContext(AppContext);
 
   const [docInfo, setDocInfo] = useState(null);
+  const [docSlots, setDocSlots] = useState([]);
+  const [slotIndex, setSlotIndex] = useState(0);
+  const [slotTime, setSlotTime] = useState("");
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find((doc) => doc._id === docId);
     setDocInfo(docInfo);
-    console.log(docInfo);
   };
+
+  const getAvailableSlots = async () => {
+    setDocSlots([]);
+    // getting current date
+    let today = new Date();
+
+    for (let i = 0; i < 7; i++) {
+      // getting date with index
+      let currentDate = new Date(today);
+      currentDate.setDate(today.getDate() + i);
+
+      // setting end time of date with index
+      let endTime = new Date();
+      endTime.setDate(endTime.getDate() + 1);
+      endTime.setHours(21, 0, 0, 0);
+      // setting hours
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(
+          currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10
+        );
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0);
+      } else {
+        currentDate.setHours(10);
+        currentDate.setMinutes(0);
+      }
+
+      let timeSlots = [];
+
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        // add slots to array
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time: formattedTime,
+        });
+
+        // increment current time
+      }
+    }
+  };
+
   useEffect(() => {
     fetchDocInfo();
   }, [doctors, docId]);
+
+  useEffect(() => {
+    getAvailableSlots();
+  }, [docInfo]);
 
   return (
     docInfo && (
