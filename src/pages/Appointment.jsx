638e95a0fc7c72d70ba320533388ce16db38c013
mdshoeb8147 <mdshoeb8147@gmail.com>
2025-02-13 15:34:49 +0530

    25051 add css to slot timing of date

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -116,13 +116,27 @@ const Appointment = () => {
         {/* booking slot*/}
         <div className="sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700">
           <p>Booking Slots</p>
-          <div className="">
+          <div className="flex gap-3 items-center w-full overflow-x-scroll mt-4">
             {docSlots.length &&
               docSlots.map((item, index) => (
-                <div className="" key={index}>
+                <div
+                  onClick={() => setSlotIndex(index)}
+                  className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${
+                    slotIndex === index
+                      ? "bg-blue-500 text-white"
+                      : "border border-gray-400"
+                  }`}
+                  key={index}
+                >
                   <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
                   <p>{item[0] && item[0].datetime.getDate()}</p>
                 </div>
+              ))}
+          </div>
+          <div className="">
+            {docSlots.length &&
+              docSlots[slotIndex].map((item, index) => (
+                <p key={index}>{item.time.toLowerCase()}</p>
               ))}
           </div>
         </div>
